8d86a369690e5317a33e6f1cb5bcf29a6e1d0930
mozhdealiakbari <37040242+mozhdealiakbari@users.noreply.github.com>
2019-04-13 23:23:05 +0200

    Update Recommender_Systems.py

--- Personalize_Recommender/Recommender_Systems.py
+++ Personalize_Recommender/Recommender_Systems.py
@@ -2,12 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 column_names = ['user_id', 'item_id', 'rating', 'timestamp']
 df = pd.read_csv('u.data', sep='\t', names=column_names)
 project_titles = pd.read_csv('Asset_Id_Titles')
-
-
 def visualize(df):
   sns.set_style('white')
   ratings =pd.DataFrame(df.groupby('title')['rating'].mean())
@@ -15,7 +12,6 @@ def visualize(df):
   ratings['rating_numbers'].hist(bins=70)
   ratings['rating'].hist(bins=70)
   sns.jointplot(x='rating', y='rating_numbers', data=ratings, alpha=0.5)
-
 def recommendation(projectname):
   projectmat = df.pivot_table(index='user_id', columns='title', values='rating')
   ratings.sort_values('rating_numbers', ascending=False)
