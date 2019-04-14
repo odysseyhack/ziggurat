22df237fd853cb7154e1a4dda16b0e4eaaf13046
Mahpare <maparz@gmail.com>
2019-04-14 11:44:05 +0200

    Update Expert_System.py

--- Expert_System/Expert_System.py
+++ Expert_System/Expert_System.py
@@ -103,20 +103,7 @@ class StartupRuleBase(KnowledgeEngine):
 
         
         
-#     @Rule(startupInfo(color='green'))
-#     def green_light(self):
-#         print("Walk")
 
-#     @Rule(startupInfo(color='red'))
-#     def red_light(self):
-#         print("Don't walk")
-
-#     @Rule(AS.startupInfo << StartupInfo(color=L('yellow') | L('blinking-yellow')))
-#     def cautious(self, light):
-#         print("Be cautious because light is", light["color"])
-
-
-# In[9]:
 
 
 engine = StartupRuleBase()
